922ca2ddf87d22a23837b28ff62a4f1ecbc17746
Conradin Laux <conradin.laux@gmail.com>
2024-05-16 14:46:29 +0200

    fix: indent lines for zen mode

--- lua/hyde/plugins/zen-mode.lua
+++ lua/hyde/plugins/zen-mode.lua
@@ -1,3 +1,17 @@
+local default_ibl = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local funky_ibl = {
+    "Comment",
+}
+
 return {
     {
         "folke/zen-mode.nvim",
@@ -5,6 +19,12 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
+            on_open = function()
+                require("ibl").update({ indent = { highlight = funky_ibl } })
+            end,
+            on_close = function()
+                require("ibl").update({ indent = { highlight = default_ibl } })
+            end,
         },
     },
     {
