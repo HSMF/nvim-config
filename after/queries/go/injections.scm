2efe378d194c5c10095562af64961151c5ac7848
Conradin Laux <conradin.laux@gmail.com>
2024-07-10 13:19:56 +0200

    feat: better go

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -4,3 +4,19 @@
   (#match? @injection.content "^// *\\@")
   (#offset! @injection.content 0 4 0 0)
   (#set! injection.language "gobra"))
+
+
+
+(
+  (call_expression
+    function: (selector_expression
+      operand: (_) @db
+      field: (field_identifier) @exec
+      )
+    arguments: (argument_list . [ (raw_string_literal) (interpreted_string_literal) ] @injection.content)
+   )
+  (#match? @db "(pool|db|tx)$")
+  (#match? @exec "^(Exec|Prepare|Query|QueryRow)$")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "sql")
+)
