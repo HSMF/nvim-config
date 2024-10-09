3270f7662eae7af83799dd4d48ae65cdaef10e17
Conradin Laux <conradin.laux@gmail.com>
2024-10-09 14:12:17 +0200

    feat: gobra icons

---
+++ lua/hyde/plugins/icons.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "nvim-tree/nvim-web-devicons",
+        opts = {
+            override = {
+                gobra = {
+                    color = "#519aba",
+                    ctermcolor = "74",
+                    icon = "",
+                    name = "Gobra",
+                },
+            },
+        },
+    },
+}
