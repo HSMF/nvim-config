f1402b3690e9568e19fd240a6daf0c19af178c4b
Conradin Laux <conradin.laux@fokus-ag.ch>
2024-10-24 16:59:45 +0200

    feat: MakeAsync cmd

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,3 +1,6 @@
+local ts_locals = require("nvim-treesitter.locals")
+local ts_utils = require("nvim-treesitter.ts_utils")
+
 vim.api.nvim_set_keymap("i", "$", "", {
     nowait = true,
     noremap = true,
@@ -5,3 +8,32 @@ vim.api.nvim_set_keymap("i", "$", "", {
         require("autotemplate").autotemplate(vim.bo.filetype)
     end,
 })
+
+vim.api.nvim_buf_create_user_command(0, "MakeAsync", function(opts)
+    local cursor_node = ts_utils.get_node_at_cursor()
+    local node = cursor_node
+    while node ~= nil do
+        if node:type() == "arrow_function" then
+            break
+        end
+        node = node:parent()
+    end
+    if node == nil then
+        print("no arrow function")
+        return
+    end
+
+    local start_row, start_col = node:range(false)
+
+    local line = vim.api.nvim_buf_get_lines(0, start_row, start_row + 1, true)[1]
+
+    local before = string.sub(line, 1, start_col)
+    local after = string.sub(line, start_col + 1)
+
+    if string.match(after, "async") ~= nil then
+        return
+    end
+
+    local new = before .. "async " .. after
+    vim.api.nvim_buf_set_lines(0, start_row, start_row + 1, true, { new })
+end, {})
