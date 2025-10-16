68c01258cc963b9dd637709d188f8f6c85abacf0
Conradin Laux <conradin.laux@fokus-ag.ch>
2025-10-16 15:10:23 +0200

    dap for cs

--- lua/hyde/plugins/dap.lua
+++ lua/hyde/plugins/dap.lua
@@ -1,5 +1,33 @@
 return {
     {
-        "mfussenegger/nvim-dap"
-    }
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            dap.adapters.coreclr = {
+                type = "executable",
+                command = "netcoredbg",
+                args = { "--interpreter=vscode" },
+            }
+
+            dap.configurations.cs = {
+                {
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "attach",
+                    console = "integratedTerminal",
+                    processId = function()
+                        return vim.fn.input("Enter process ID: ")
+                    end,
+                    -- program = function()
+                    --     return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/net9.0", "file")
+                    -- end,
+                },
+            }
+        end,
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        opts = {},
+        { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+    },
 }
