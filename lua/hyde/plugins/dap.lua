5bacfde15a3c86c95ff855d1614179c31bcc9d6d
Conradin Laux <conradin.laux@fokus-ag.ch>
2025-10-20 14:15:48 +0200

    add launch request

--- lua/hyde/plugins/dap.lua
+++ lua/hyde/plugins/dap.lua
@@ -5,14 +5,17 @@ return {
             local dap = require("dap")
             dap.adapters.coreclr = {
                 type = "executable",
-                command = "netcoredbg",
+                command = vim.fn.exepath("netcoredbg"),
                 args = { "--interpreter=vscode" },
+                options = {
+                    detached = false,
+                }
             }
 
             dap.configurations.cs = {
                 {
                     type = "coreclr",
-                    name = "launch - netcoredbg",
+                    name = "attach - netcoredbg",
                     request = "attach",
                     console = "integratedTerminal",
                     processId = function()
@@ -22,6 +25,22 @@ return {
                     --     return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/net9.0", "file")
                     -- end,
                 },
+                {
+                    type = "coreclr",
+                    name = "launch",
+                    request = "launch",
+                    program = function()
+                        local suggestion = vim.g.last_dll
+                        if suggestion == nil then
+                            suggestion = vim.fn.getcwd() .. '/bin/Debug/'
+                        end
+                        local ret = vim.fn.input('Path to dll: ', suggestion, 'file')
+                        if ret then
+                            vim.g.last_dll = ret
+                        end
+                        return ret
+                    end
+                }
             }
         end,
     },
